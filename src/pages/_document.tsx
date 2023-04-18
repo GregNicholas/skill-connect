a8a58343c0fdd6457b4b7586d33d2a50c31d1c4d
GregNicholas <59461870+GregNicholas@users.noreply.github.com>
2023-04-18 03:39:17 +0000

    setup document pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Skill connect where organizations can find talented help and manage applications"></meta>
+        <link rel="icon" href="/GSLogo11.png"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
