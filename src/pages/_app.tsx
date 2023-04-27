f0534ea8fab30805c56c895710852be752045f7c
GregNicholas <59461870+GregNicholas@users.noreply.github.com>
2023-04-27 03:08:26 +0000

    bring session provider to _app.tsx to wrap app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,22 @@ import Layout from '@/components/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import {SessionProvider} from 'next-auth/react'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ 
+  Component, 
+  pageProps: { session, ...pageProps }, 
+}: AppProps) {
   return (
     <>
+    <SessionProvider session={session}>
       <Head>
         <title>Skill Connect</title>
       </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+    </SessionProvider>
     </>
   )
 }
