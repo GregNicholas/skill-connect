82296ad2af371d7017a056b3c2cc6636a07b8c88
Greg Schoenberg <gregschoenberg@gmail.com>
2023-04-27 22:43:12 -0700

    adding optional dynamic field inputs to form

--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -1,4 +1,5 @@
-import { ChangeEvent, FormEvent } from 'react';
+import axios from 'axios';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 interface Props {
     handleSubmit: (e: FormEvent<HTMLFormElement>, title: string, description: string) => Promise<void> 
@@ -9,7 +10,32 @@ interface Props {
     }
 }
 
-export default function NewPostForm({ formInfo, handleSubmit, handleChange }: Props) {    
+// export default function NewPostForm({ formInfo, handleSubmit, handleChange }: Props) {    
+export default function NewPostForm() {    
+
+    const [formInfo, setFormInfo] = useState({title: "", description: "", fields: [{label: ""}]});
+
+    const handleSubmit  = async (e: FormEvent<HTMLFormElement>, title: string, description: string) => {
+        e.preventDefault();
+        const { data } = await axios.post("/api/posts", {title, description});
+        console.log(data);
+        setFormInfo({title: "", description: "", fields: [{label: ""}]});
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setFormInfo({...formInfo, [e.target.name]: e.target.value});
+    }
+
+    const addInputField = () => {
+        const newFields = [...formInfo.fields, {label: ""}];
+        setFormInfo((prev) => ({...prev, fields: newFields}));
+    }
+
+    const removeInputField = (index: number) => {
+        const newFields = [...formInfo.fields];
+        newFields.splice(index, 1);
+        setFormInfo((prev) => ({...prev, fields: newFields}));
+    }
 
     const inputStyle = "outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-300 text-slate-900 p-2 w-full max-w-[40ch]"
     return (
@@ -39,6 +65,25 @@ export default function NewPostForm({ formInfo, handleSubmit, handleChange }: Pr
                     aria-label="description"
                     required
                 />
+                {
+                    formInfo.fields.map((data, index) => {
+                        const {label} = data;
+                        return (
+                            <div className="h-10 flex items-center w-full max-w-[40ch] mb-3" key={index}>
+                            <input className={inputStyle}
+                                onChange={(e) => handleChange(e)}
+                                value={label}
+                                type="text" 
+                                placeholder={"e.g. Make coffee"} 
+                                aria-label="new subtask"
+                                required
+                            /> 
+                            <button type="button" className="text-2xl font-bold" onClick={(e)=> removeInputField(index)}>x</button>
+                            </div>
+                        )
+                    })
+                }
+                <button className="px-4 py-2" onClick={addInputField}>Add an input</button>
                 <button 
                     type="submit" 
                     className="bg-cyan-300 hover:bg-cyan-400 text-white font-bold py-2 px-4 rounded"
--- src/pages/addPost.tsx
+++ src/pages/addPost.tsx
@@ -3,22 +3,24 @@ import NewPostForm from "@/components/NewPostForm";
 import { ChangeEvent, FormEvent, useState } from 'react';
 
 export default function AddPost() {
-    const [formInfo, setFormInfo] = useState({title: "", description: ""});
+    // const [formInfo, setFormInfo] = useState({title: "", description: "", fields: [{label: ""}]});
 
-    const handleSubmit  = async (e: FormEvent<HTMLFormElement>, title: string, description: string) => {
-        e.preventDefault();
-        const { data } = await axios.post("/api/posts", {title, description});
-        console.log(data);
-        setFormInfo({title: "", description: ""});
-    }
+    // const handleSubmit  = async (e: FormEvent<HTMLFormElement>, title: string, description: string) => {
+    //     e.preventDefault();
+    //     const { data } = await axios.post("/api/posts", {title, description});
+    //     console.log(data);
+    //     setFormInfo({title: "", description: "", fields: [{label: ""}]});
+    // }
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setFormInfo({...formInfo, [e.target.name]: e.target.value});
-    }
+    // const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    //     setFormInfo({...formInfo, [e.target.name]: e.target.value});
+    // }
 
     return (
         <div>
-            <NewPostForm formInfo={formInfo} handleSubmit={handleSubmit} handleChange={handleChange} />
+            <div>Fill out the form below to customize your post</div>
+            {/* <NewPostForm formInfo={formInfo} handleSubmit={handleSubmit} handleChange={handleChange} /> */}
+            <NewPostForm />
         </div>
     )
 }
