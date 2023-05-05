1e6990818c5dd400885886ee019fa63e4f140edd
Greg Schoenberg <gregschoenberg@gmail.com>
2023-05-04 22:55:23 -0700

    form with dynamic inputs is working for update and submit (only console logging extra inputs on submit until schema is updated

--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -13,21 +13,31 @@ interface Props {
 // export default function NewPostForm({ formInfo, handleSubmit, handleChange }: Props) {    
 export default function NewPostForm() {    
 
-    const [formInfo, setFormInfo] = useState({title: "", description: "", fields: [{label: ""}]});
+    const [formInfo, setFormInfo] = useState({title: "", description: "", fields: [{val: "", name: "input0"}]});
 
-    const handleSubmit  = async (e: FormEvent<HTMLFormElement>, title: string, description: string) => {
+    const handleSubmit  = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        const {title, description, fields} = formInfo;
+        console.log(fields)
         const { data } = await axios.post("/api/posts", {title, description});
         console.log(data);
-        setFormInfo({title: "", description: "", fields: [{label: ""}]});
+        setFormInfo({title: "", description: "", fields: [{val: "", name: "input0"}]});
     }
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setFormInfo({...formInfo, [e.target.name]: e.target.value});
+        const { name, value } = e.target;
+        if (name.includes("input")) {
+            const inputIndex = parseInt(name.replace("input", ""));
+            const updatedFields = [...formInfo.fields];
+            updatedFields[inputIndex].val = value;
+            setFormInfo({ ...formInfo, fields: updatedFields });
+        } else {
+            setFormInfo({ ...formInfo, [name]: value });
+        }
     }
 
     const addInputField = () => {
-        const newFields = [...formInfo.fields, {label: ""}];
+        const newFields = [...formInfo.fields, {val: "", name: `input${formInfo.fields.length}`}];
         setFormInfo((prev) => ({...prev, fields: newFields}));
     }
 
@@ -41,13 +51,14 @@ export default function NewPostForm() {
     return (
         <>
             <form 
-                onSubmit={(e) => handleSubmit(e, formInfo.title, formInfo.description)}
+                onSubmit={(e) => handleSubmit(e)}
                 className="text-xs sm:text-sm flex-1 flex flex-col gap-4 justify-center items-center"
             >
                 <input 
-                    value={formInfo.title}
                     onChange={handleChange}
+                    id=""
                     name="title"
+                    value={formInfo.title}
                     type="text" 
                     placeholder="Title..." 
                     className={inputStyle} 
@@ -55,9 +66,9 @@ export default function NewPostForm() {
                     required
                 />
                 <input 
-                    value={formInfo.description}
                     onChange={handleChange}
                     id="description"
+                    value={formInfo.description}
                     name="description"
                     type="text" 
                     placeholder="Description..." 
@@ -67,12 +78,14 @@ export default function NewPostForm() {
                 />
                 {
                     formInfo.fields.map((data, index) => {
-                        const {label} = data;
+                        const {val, name} = data;
                         return (
                             <div className="h-10 flex items-center w-full max-w-[40ch] mb-3" key={index}>
                             <input className={inputStyle}
                                 onChange={(e) => handleChange(e)}
-                                value={label}
+                                id={name}
+                                value={val}
+                                name={name}
                                 type="text" 
                                 placeholder={"e.g. Make coffee"} 
                                 aria-label="new subtask"
@@ -85,12 +98,12 @@ export default function NewPostForm() {
                 }
                 <button 
                     type="button" 
-                    className="px-4 py-2 border-solid rounded-xl border-slate-300" 
+                    className="px-4 py-2 border-2 border-slate-300 rounded-xl hover:bg-slate-800" 
                     onClick={addInputField}
                 >Add an input</button>
                 <button 
                     type="submit" 
-                    className="bg-slate-300 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded"
+                    className="bg-slate-800 hover:bg-slate-300 hover:text-slate-800 text-white font-bold py-2 px-4 rounded-xl"
                 >Submit</button>
             </form>
         </>
